2c59f674098d42c571642d67931dcae916429741
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-07 04:04:02 +0300

    nested-routes-101

---
+++ src/app/blog/blog1/page.tsx
@@ -0,0 +1,3 @@
+export default function Blog1() {
+    return <h1>This is the Blog 1 Page</h1>;
+}
---
+++ src/app/blog/blog2/page.tsx
@@ -0,0 +1,3 @@
+export default function Blog2() {
+    return <h1>This is the Blog 2 Page</h1>;
+}
