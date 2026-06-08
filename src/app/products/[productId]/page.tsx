63a6a341f4c16989717c6bb0705d41bccda81eeb
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-08 18:41:12 +0300

    dynamic routes 101

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductDetail( {
+    params,
+}: {
+    params: Promise<{ productId: string }>;
+}) {
+    const productId = (await params).productId;
+    return <h1> This is product: {productId}</h1>;
+}
+
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+export default function ProductsList() {
+    return (
+        <>
+            <h1>Products List</h1>
+            <ol>
+                <li>Product 1</li>
+                <li>Product 2</li>
+                <li>Product 3</li>
+                <li>Product 4</li>
+                <li>Product 5</li>
+            </ol>
+        </>
+    );
+}
