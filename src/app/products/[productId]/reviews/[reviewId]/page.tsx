a098a90a45df719362b460110cb00ce057170717
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-09 12:11:58 +0300

    dynamic nested routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function ReviewDetail( {
+    params,
+}: {
+    params: Promise<{ reviewId: string; productId: string }>;
+}) {
+    const {productId, reviewId} = await params
+    return <h1> This is review: {reviewId} for product: {productId}</h1>;
+}
