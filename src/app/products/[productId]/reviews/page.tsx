3e3b43081371fa0d7c368a3a35c109ee0b871872
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-09 12:44:58 +0300

    static review sample list

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,12 @@
+export default async function ReviewsSampleList() {
+    return (
+        <div>
+            <h1>Reviews Sample List</h1>
+            <ul>
+                <li>Review 1</li>
+                <li>Review 2</li>
+                <li>Review 3</li>
+            </ul>
+        </div>
+    );
+}
