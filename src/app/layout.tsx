a7d84418f4bb19a9d97918355435498648697370
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-11 02:03:13 +0300

    layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,32 @@
-//Root layout
+//Root layout file
 import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Global Header */}
+        <header
+          style={{
+            backgroundColor: "#ff7a21",
+            padding: "0.5rem",
+          }}
+        >
+          <p style={{ fontWeight: "bold" }} >Header Sample</p>
+        </header>
+
+        {children}
+
+        {/* Global Footer */}
+        <footer
+          style={{
+            backgroundColor: "black",
+            padding: "0.5rem",
+          }}
+        >
+          <p style={{ color: "white", fontWeight: "bold"}} >Footer Sample</p>
+        </footer>
+      </body>
     </html>
   );
 }
