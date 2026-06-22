4cf16ff20b01da9aa701c2e3a7529b2d6c0d8fb6
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-23 00:02:01 +0300

    handling POST requests

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,8 +1,25 @@
 // Route handler to define and export the GET handler function
 
-import { comments } from "./data"; //import comment data
+//Import comment data
+import { comments } from "./data"; 
 
 // GET Fn returns comments array as a JSON response
 export async function GET() {
     return Response.json(comments);
 } 
+
+// POST fn - handles incoming data and creates a new comment
+export async function POST(request: Request) {
+    const comment = await request.json();
+
+    //Create new comment and generate new id automatically
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text,
+    };
+    comments.push(newComment); //temp in-memory array
+    return new Response(JSON.stringify(newComment), {
+        headers: { "Content-Type": "application/json" },
+        status: 201,
+    });
+}
