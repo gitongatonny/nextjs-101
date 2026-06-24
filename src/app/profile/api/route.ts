b1ead4d14a0e96a8526fcde62dda2d78ad6df38d
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-24 04:24:25 +0300

    headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,22 @@
-// Basic Route Handler Organization - place it in an 'api' folder to prevent it from taking precedence over the page.tsx file in the same folder.
+import  { type NextRequest } from "next/server"; 
 
-export async function GET(){
-    return new Response("Profile Data");
+import { headers } from "next/headers";
+
+// Basic Route Handler Organization - place it in an 'api' folder to prevent it from taking precedence over the page.tsx file in the same folder.
+export async function GET(request: NextRequest){
+    //Method 1. (Using request param) extract request headers
+    //const requestHeaders = new Headers(request.headers);
+    //show Authorization header's value
+    //console.log(requestHeaders.get("Authorization"));
+
+    //Method 2. Headers fn from Next.js
+    const headersList = await headers();
+    console.log(headersList.get("Authorization"));
+
+    return new Response("<h1>Profile Data</h1>", {
+        //Sending custom header to convert it from text to html
+        headers: {
+            "Content-Type": "text/html",
+        },
+    });
 }
