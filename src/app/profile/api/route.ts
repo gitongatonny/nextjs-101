99733798e2f532ea0511b5e2b056c41096b3596d
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-06-25 07:31:22 +0300

    cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,6 +1,6 @@
 import  { type NextRequest } from "next/server"; 
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 // Basic Route Handler Organization - place it in an 'api' folder to prevent it from taking precedence over the page.tsx file in the same folder.
 export async function GET(request: NextRequest){
@@ -13,10 +13,22 @@ export async function GET(request: NextRequest){
     const headersList = await headers();
     console.log(headersList.get("Authorization"));
 
+    //cookie request obj
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    //Next.js cookie fn
+    const cookiesStore = await cookies()
+    cookiesStore.set("resultsPerPage", "20") //set cookie
+    console.log(cookiesStore.get("resultsPerPage"));
+
     return new Response("<h1>Profile Data</h1>", {
         //Sending custom header to convert it from text to html
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark",
         },
     });
 }
+
+//Cookies
