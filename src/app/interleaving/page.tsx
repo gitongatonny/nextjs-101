5fcf533f94256ade4fe83c479f695ddb60c9424a
Tonny <118627853+gitongatonny@users.noreply.github.com>
2026-07-04 22:33:52 +0300

    interleaving server & client components

---
+++ src/app/interleaving/page.tsx
@@ -0,0 +1,17 @@
+
+import { ServerComponentOne } from "@/components/server-component-one";
+
+import { ClientComponentOne } from "@/components/client-component-one";
+
+export default function InterleavingPage() {
+    return (
+        <>
+            <h1>Interleaving Page</h1>
+
+            <ClientComponentOne>
+                <ServerComponentOne />
+            </ClientComponentOne>
+
+        </>
+    );
+}
---
+++ src/components/server-component-one.tsx
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+//import { ServerComponentTwo } from "./server-component-two";
+
+import { ClientComponentOne } from "./client-component-one";
+
+export const ServerComponentOne = () => {
+  fs.readFileSync("src/components/server-component-one.tsx", "utf8");
+
+  return (
+    <>
+      <h1>Server Component One</h1>
+
+      {/*<ClientComponentOne />*/}
+
+      {/*<ServerComponentTwo />*/}
+    </>
+  );
+};
